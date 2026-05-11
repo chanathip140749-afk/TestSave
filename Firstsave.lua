87ba3ec837247a2a8f3f9ec621e80cb3455160bd
chanathip140749-afk <chanathip140749@gmail.com>
2026-05-11 22:18:30 +0700

    Update Firstsave.lua

--- Firstsave.lua
+++ Firstsave.lua
@@ -1,3 +1,19 @@
-local path = game:GetService("ReplicatedStorage").Config.SpeedRobux
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["1"]
 local mod = require(path)
-mod.
+mod.BaseDamage = { 150, 250 }
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["2"]
+local mod = require(path)
+mod.BaseDamage = { 150, 250 }
+
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["3"]
+local mod = require(path)
+mod.BaseDamage = { 150, 250 }
+
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["4"]
+local mod = require(path)
+mod.BaseDamage = { 150, 250 }
+
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["5"]
+local mod = require(path)
+mod.BaseDamage = { 150, 250 }
+
