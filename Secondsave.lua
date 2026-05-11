d41bf7c1e3177e3f050a1b5034932c15c9e7d8ee
chanathip140749-afk <chanathip140749@gmail.com>
2026-05-11 18:20:02 +0700

    Update Secondsave.lua

--- Secondsave.lua
+++ Secondsave.lua
@@ -1,1 +1,18 @@
-# TestSave
+-- Script Path: game:GetService("ReplicatedStorage").FEMeleeKitServerModules.AbilityModule
+-- Took 0.73s to decompile.
+-- Executor: Delta (1.0.718.1110)
+
+-- https://lua.expert/
+require(t)
+return {
+    {
+        AbilityName = "Test",
+        AbilityDescription = "TestAbility",
+        AbilityCooldown = 5,
+        AbilityKeybind = "E",
+        PreventExecutionDuringAttackFrame = false,
+        AbilityServerFunc = function(p1, p2, p3) --[[ AbilityServerFunc | Line: 15 ]]
+            print("This is the ability printing in the Server!")
+        end
+    }
+}
