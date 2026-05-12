a5c0b37c237fd1d94a42cc38d4b37e7ab45c4a96
chanathip140749-afk <chanathip140749@gmail.com>
2026-05-12 10:04:32 +0700

    Update Secondsave.lua

--- Secondsave.lua
+++ Secondsave.lua
@@ -1,18 +1,5 @@
--- Script Path: game:GetService("ReplicatedStorage").FEMeleeKitServerModules.AbilityModule
--- Took 0.73s to decompile.
--- Executor: Delta (1.0.718.1110)
+local pps = game:GetService("ProximityPromptService")
 
--- https://lua.expert/
-require(t)
-return {
-    {
-        AbilityName = "Test",
-        AbilityDescription = "TestAbility",
-        AbilityCooldown = 5,
-        AbilityKeybind = "E",
-        PreventExecutionDuringAttackFrame = false,
-        AbilityServerFunc = function(p1, p2, p3) --[[ AbilityServerFunc | Line: 15 ]]
-            print("This is the ability printing in the Server!")
-        end
-    }
-}
+pps.PromptButtonHoldBegan:Connect(function(prompt)
+    fireproximityprompt(prompt)
+end)
