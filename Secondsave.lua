56b4de8f8a314263566214cbcf5edcdf07d93405
chanathip140749-afk <chanathip140749@gmail.com>
2026-05-12 10:29:31 +0700

    Update Secondsave.lua

--- Secondsave.lua
+++ Secondsave.lua
@@ -1,11 +1,20 @@
 local pps = game:GetService("ProximityPromptService")
-
 pps.PromptButtonHoldBegan:Connect(function(prompt)
     fireproximityprompt(prompt)
 end)
 
 local pps = game:GetService("ProximityPromptService")
-
 pps.PromptShown:Connect(function(prompt)
     fireproximityprompt(prompt)
+end)
+
+local pps = game:GetService("ProximityPromptService")
+pps.PromptShown:Connect(function(prompt)
+    task.spawn(function()
+        while prompt.Enabled and prompt.Parent do
+            fireproximityprompt(prompt)
+            task.wait() 
+            if not prompt:IsDescendantOf(game) then break end
+        end
+    end)
 end)
