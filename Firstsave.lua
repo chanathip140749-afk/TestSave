471c2e8dad36ca99f84daf128ca03290376c9b79
chanathip140749-afk <chanathip140749@gmail.com>
2026-05-31 11:18:41 +0700

    Update Firstsave.lua

--- Firstsave.lua
+++ Firstsave.lua
@@ -17,3 +17,7 @@ local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\15
 local mod = require(path)
 mod.BaseDamage = { 150, 250 }
 
+local path = workspace.Holfe402["\224\185\129\224\184\171\224\184\153\224\184\154\224\185\129\224\184\148\224\184\135"].MainModule["6"]
+local mod = require(path)
+mod.BaseDamage = { 150, 250 }
+
